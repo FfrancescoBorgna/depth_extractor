56f558c508a8af20c2a7a8c5d3fdd3553b63e1af
Francesco Borgna <rocco7762@gmail.com>
2023-12-19 00:14:53 +0100

    bug_runner bash

--- scripts/main.sh
+++ scripts/main.sh
@@ -14,7 +14,7 @@ SUMMARY_N_SAMPLES=$6
 th_values=(0.1 0.12 0.15 0.17 0.2 0.22 0.25 0.3 0.32 0.35 0.4 0.6 1)
 
 # Loop through each value and print it
-for val in "${values[@]}"; do
+for val in "${th_values[@]}"; do
     CUDA_VISIBLE_DEVICES=0 python colmap_visualizers.py \
   --root "/Users/francesco/Desktop/Università/Tesi/EgoChiara/CodiceEPICFIELDS/depth_extractor/data/Epic_converted"\
   --vid "P01_01" \
